f8c726c225c162a2810201076cd6d2b00eefebeb
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-02 16:56:03 -0300

    chore: remove import

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,7 +1,6 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { ObjectId } from 'mongodb'
 
 import prisma from "@/lib/db";
 
