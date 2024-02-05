ee9cdac26aec0b042c63b8df2d58ccfdbf5835cf
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-04 22:33:47 -0300

    feat: CartContext and types

---
+++ src/context/CartContext.tsx
@@ -0,0 +1,65 @@
+'use client'
+import { createContext, useState, useEffect, ReactNode } from "react";
+import { ProductProps, CartContextProps } from '@/intefaces/AllInterfaces'
+
+export const CartContext = createContext<CartContextProps>({ cart: [], addCart: () => {}, addQtdCart: () => {}, removeQtdCart: () => {}, qtdTotal: 0, totalValue: 0});
+
+export const CartProvider = ({ children } : { children : ReactNode }) => {
+
+    const [cart,setCart] = useState<ProductProps[]>([])
+    const [qtdTotal, setQtdTotal] = useState<number>(0)
+    const [totalValue, setTotalValue] = useState<number>(0)
+
+    function addCart(product: ProductProps) {
+        if (cart.find((item) => item.id === product.id)) {
+            addQtdCart(product)
+            return
+        }
+        setCart([...cart, product])
+    }
+
+    function addQtdCart(product: ProductProps) {
+        const newCart = cart.map((item) => {
+            if(item.id === product.id) {
+                item.qtd += 1
+            }
+            return item
+        })
+        setCart(newCart)
+    }
+
+    function removeQtdCart(product: ProductProps) {
+        const newCart = cart.map((item) => {
+            if(item.id === product.id) {
+                item.qtd -= 1
+            }
+            return item
+        })
+        setCart(newCart)
+    }
+
+    useEffect(() => {
+        function totalCart() {
+            let total = 0
+            cart.map((item) => {
+                total += item.qtd
+            })
+            setQtdTotal(total)
+        }
+        function totalValue(){
+            let total = 0
+            cart.map((item) => {
+                total += item.qtd * item.price
+            })
+            setTotalValue(total)
+        }
+        totalValue()
+        totalCart()
+    }, [cart])
+
+    return (
+        <CartContext.Provider value={{addCart, addQtdCart, removeQtdCart, cart, qtdTotal, totalValue}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
--- src/intefaces/AllInterfaces.ts
+++ src/intefaces/AllInterfaces.ts
@@ -5,3 +5,34 @@ export interface UserProps{
     image?: string;
     role?: string;
 }
+
+export interface ProductProps{
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+    color?: string;
+    category: CategoryProps;
+    qtd: number;
+    status: boolean;
+    createdAt: Date;
+    updatedAt: Date;
+    categoryId: string;
+}
+
+interface CategoryProps{
+    id: string;
+    name: string;
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+export interface CartContextProps{
+    cart: ProductProps[]
+    addCart: (product: ProductProps) => void
+    addQtdCart: (product: ProductProps) => void
+    removeQtdCart: (product: ProductProps) => void
+    qtdTotal: number
+    totalValue: number
+}
