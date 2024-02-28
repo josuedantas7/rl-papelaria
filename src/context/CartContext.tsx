7ba3b73204fc54da3ab6fcf58aec09de05a60c5d
Josué Dantas <105951027+wDarkRx@users.noreply.github.com>
2024-02-28 19:55:25 -0300

    update: CartContext

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,45 +1,127 @@
 'use client'
 import { createContext, useState, useEffect, ReactNode } from "react";
-import { ProductProps, CartContextProps } from '@/intefaces/AllInterfaces'
+import { ProductProps, CartContextProps, CartProps } from '@/intefaces/AllInterfaces'
+import { useSession } from "next-auth/react";
+import { api } from "@/lib/api";
 
-export const CartContext = createContext<CartContextProps>({ cart: [], addCart: () => {}, addQtdCart: () => {}, removeQtdCart: () => {}, qtdTotal: 0, totalValue: 0});
+export const CartContext = createContext<CartContextProps>({ cart: [], addCart: () => {}, addQtdCart: () => {}, removeQtdCart: () => {}, qtdTotal: 0, setCart: () => {}});
 
 export const CartProvider = ({ children } : { children : ReactNode }) => {
 
-    const [cart,setCart] = useState<ProductProps[]>([])
+    const [cart,setCart] = useState<CartProps[]>([])
     const [qtdTotal, setQtdTotal] = useState<number>(0)
-    const [totalValue, setTotalValue] = useState<number>(0)
+    const { data:session, status } = useSession()
+
+    const [cartExists, setCartExists] = useState<boolean>(false)
 
     function addCart(product: ProductProps) {
-        if (cart.find((item) => item.id === product.id)) {
-            addQtdCart(product)
-            return
+        if (cartExists) {
+            const productExistsInCart = cart.find((item) => item.product.id === product.id)
+            if (productExistsInCart) {
+                setCart([...cart, {
+                    qtd: productExistsInCart.qtd + 1,
+                    product: product as {} as ProductProps,
+                }])
+            } else {
+                setCart([...cart, {
+                    qtd: 1,
+                    product: product as {} as ProductProps,
+                }])
+            }
+        }else {
+            const productExistsInCart = cart.find((item) => item.product.id === product.id)
+            if (productExistsInCart) {
+                setCart(cart.map((item) => {
+                    if(item.product.id === product.id) {
+                        item.qtd += 1
+                    }
+                    return item
+                }))
+            } else {
+                setCart([...cart, {
+                    qtd: 1,
+                    product: product as {} as ProductProps,
+                }])
+            }
         }
-        setCart([...cart, product])
     }
 
-    function addQtdCart(product: ProductProps) {
-        const newCart = cart.map((item) => {
-            if(item.id === product.id && item.qtd) {
-                item.qtd += 1
+    function addQtdCart(product: CartProps) {
+        if (cartExists) {
+            const productExistsInCart = cart.find((item) => item.product.id === product.product.id)
+            console.log('ITEM REPERTIDO')
+            if (productExistsInCart) {
+                setCart(cart.map((item) => {
+                    if(item.product.id === product.product.id) {
+                        item.qtd += 1
+                    }
+                    return item
+                }))
+            } else {
+                console.log('ITEM NAO REPETIDO')
+                setCart([...cart, {
+                    qtd: 1,
+                    product: product.product,
+                }])
             }
-            return item
-        })
-        setCart(newCart)
+        } else {
+            const productExistsInCart = cart.find((item) => item.product.id === product.product.id)
+            if (productExistsInCart) {
+                setCart(cart.map((item) => {
+                    if(item.product.id === product.product.id) {
+                        item.qtd += 1
+                    }
+                    return item
+                }))
+            } else {
+                setCart([...cart, {
+                    qtd: 1,
+                    product: product.product,
+                }])
+            }
+        }
     }
 
-    function removeQtdCart(product: ProductProps) {
-        if (product.qtd === 1) {
-            setCart(cart.filter((item) => item.id !== product.id))
-            return
-        }
-        const newCart = cart.map((item) => {
-            if(item.id === product.id && item.qtd) {
-                item.qtd -= 1
+    function removeQtdCart(product: CartProps) {
+        if (cartExists) {
+            const productExistsInCart = cart.find((item) => item.product.id === product.product.id)
+            if (productExistsInCart) {
+                setCart(cart.map((item) => {
+                    if(item.product.id === product.product.id) {
+                        item.qtd -= 1
+                        if (item.qtd === 0) {
+                            setCart(cart.filter((item) => item.product.id !== product.product.id))
+                            
+                        }
+                    }
+                    return item
+                }))
+            } else {
+                setCart([...cart, {
+                    qtd: 1,
+                    product: product.product,
+                }])
             }
-            return item
-        })
-        setCart(newCart)
+        } else {
+            const productExistsInCart = cart.find((item) => item.product.id === product.product.id)
+            if (productExistsInCart) {
+                const updatedCart = cart.map((item) => {
+                    if (item.product.id === product.product.id) {
+                        if (item.qtd === 1) {
+                            console.log('filter sem cart salvo');
+                            return null;
+                        }
+                        return { ...item, qtd: item.qtd - 1 };
+                    }
+                    return item;
+                }).filter(Boolean);
+            
+                setCart(updatedCart as CartProps[]);
+            } else {
+                // Adiciona um novo item ao carrinho
+                setCart([...cart, { qtd: 1, product: product.product }]);
+            }
+        }
     }
 
     useEffect(() => {
@@ -51,20 +133,26 @@ export const CartProvider = ({ children } : { children : ReactNode }) => {
             })
             setQtdTotal(total)
         }
-        function totalValue(){
-            let total = 0
-            cart.map((item) => {
-                if (!item.qtd) item.qtd = 1
-                total += item.qtd * item.price
-            })
-            setTotalValue(total)
-        }
-        totalValue()
         totalCart()
     }, [cart])
 
+    useEffect(() => {
+        if (status === 'authenticated') {
+            const getCartByUser = async () => {
+                const response = await api.get('/api/cart')
+                if (response.data.length > 0) {
+                    setCartExists(true)
+                }
+                const carrinho = response.data.cart.cartProducts
+                console.log(carrinho)
+                setCart(carrinho)
+            }
+            getCartByUser()
+        }
+    },[session,status, setCart])
+
     return (
-        <CartContext.Provider value={{addCart, addQtdCart, removeQtdCart, cart, qtdTotal, totalValue}}>
+        <CartContext.Provider value={{addCart, addQtdCart, removeQtdCart, cart, qtdTotal, setCart}}>
             {children}
         </CartContext.Provider>
     )
